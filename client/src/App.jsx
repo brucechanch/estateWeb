575a56547c8b86c284bb393e8609656b73fec850
Brucechan <brucechanch@gmail.com>
2023-10-19 14:32:52 +0800

    Create the header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,12 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 export default function App() {
-  return <BrowserRouter>
+  return (
+  <BrowserRouter>
+  <Header />
   <Routes>
     <Route path="/" element= {<Home />} />
     <Route path="/sign-in" element= {<SignIn/>} />
@@ -16,4 +19,5 @@ export default function App() {
 
   </Routes>
   </BrowserRouter>
+  )
 }
