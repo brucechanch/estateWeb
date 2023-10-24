be327e4a156caa247489b9b06e87e6a6261d75d9
Brucechan <brucechanch@gmail.com>
2023-10-24 22:39:48 +0800

    delete item done

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -126,6 +126,23 @@ const handleShowListings = async() => {
 setShowListingsError(true)
   }
 }
+const handleListingDelete = async(listingId) => {
+  try {
+const res = await fetch(`/api/listing/delete/${listingId}`, {
+  method: 'DELETE',
+})
+const data =await res.json();
+if (data.success === false) {
+  console.log(data.message)
+  return;
+}
+
+setUserListings((prev) =>
+prev.filter((listing) => listing._id !== listingId))
+  } catch(error) {
+    console.log(error.message)
+  }
+}
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -170,7 +187,7 @@ setShowListingsError(true)
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700">Delete Account </span>
-         <span onClick={handleSignOut}className="text-red-700">Sign Out </span>
+        <span onClick={handleSignOut}className="text-red-700">Sign Out </span>
       </div>
       <p className="text-red-700 mt-5 ">{error? error : ''}</p>
       <p className="text-green-700 mt-5">{updateSuccess ? 'User is updated successfully ! ' : ''}</p>
@@ -189,7 +206,7 @@ setShowListingsError(true)
                     <p className=''> {listing.name}</p>
                   </Link>
                   <div className="flex flex-col items-center">
-                <button className="text-red-700">DELETE</button>
+                <button onClick={() => handleListingDelete(listing._id)} className="text-red-700">DELETE</button>
                 <button className="text-green-700">EDIT</button>
                   </div>
                   </div>
