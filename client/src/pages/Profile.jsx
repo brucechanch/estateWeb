2b3bfbaeba8c6e70e5c90148a97ae4e92471983d
Brucechan <brucechanch@gmail.com>
2023-10-24 16:23:33 +0800

    delete user

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,9 @@ import { useSelector} from 'react-redux'
 import { useRef, useState, useEffect } from 'react'
 import { getStorage, uploadBytesResumable, ref, getDownloadURL  } from 'firebase/storage'
 import { app } from '../firebase'
-import {updateUserStart, updateUserSuccess, updateUserFailure} from '../redux/user/userSlice'
+import {updateUserStart, updateUserSuccess, updateUserFailure,  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,} from '../redux/user/userSlice'
 import { useDispatch } from 'react-redux'
 export default function Profile() {
   const fileRef = useRef(null)
@@ -77,7 +79,22 @@ uploadTask.on(
     }
   };
 
-
+const handleDeleteUser = async () => {
+  try {
+    dispatch(deleteUserStart())
+    const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+method: 'DELETE',
+    })
+    const data = await res.json()
+    if(data.success === false) {
+      dispatch(deleteUserFailure(data.message));
+      return
+    }
+    dispatch(deleteUserSuccess(''))
+  } catch (error){
+    dispatch(deleteUserFailure(error.message))
+  }
+}
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -121,7 +138,7 @@ uploadTask.on(
 
       </form>
       <div className="flex justify-between mt-5">
-        <span className="text-red-700">Delete Account </span>
+        <span onClick={handleDeleteUser} className="text-red-700">Delete Account </span>
          <span className="text-red-700">Sign Out </span>
       </div>
       <p className="text-red-700 mt-5 ">{error? error : ''}</p>
